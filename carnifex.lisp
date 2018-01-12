2de2408351cd052fe7b7136a8c74fde70f7fc3d8
Clement Trosa <me@trosa.io>
2018-01-12 16:45:42 +0100

    debug line test

--- carnifex.lisp
+++ carnifex.lisp
@@ -5,6 +5,21 @@
 (ql:quickload "unix-opts"
   :silent t)
 
+(defun len (list)
+  (if list
+	  (1+ (len (cdr list)))
+	0))
+
+(defun usage ()
+  (format t "usage: carniflex <width> <height>~%"))
+
 (defun main ()
-  (format t "Hello world.~%")
+  (let* ((al (len *posix-argv*)))
+	(if (not (= al 3))
+		(usage)
+	  nil))
+  (let* ((width (nth 1 *posix-argv*))
+		 (height (nth 2 *posix-argv*)))
+	(format t "Width: ~f, Height: ~f~%"
+			width height))
   (sb-ext:exit))
