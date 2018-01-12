7f7c4165a8844c24843c4ded921d259228edb2a4
Clement Trosa <me@trosa.io>
2018-01-12 15:36:46 +0100

    Committing in accordance with the prophecy.

---
+++ Makefile
@@ -0,0 +1,2 @@
+all:
+	sbcl --noinform --load carnifex.lisp --end-toplevel-options 20 20
--- carnifex.lisp
+++ carnifex.lisp
@@ -1,4 +1,8 @@
 ;; carnifex.lisp
 ;; 2018 - iomonad <me@trosa.io>
 
-(ql:quickload "lispbuilder-sdl")
+;; (ql:quickload "lispbuilder-sdl")
+(ql:quickload "unix-opts")
+
+(defun main ()
+  "Application entry point")
