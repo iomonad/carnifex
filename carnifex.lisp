6331cbb7e105d050ef6e91c02d9b30424e9531fc
Clement Trosa <me@trosa.io>
2018-03-04 11:21:00 +0100

    arrrggghhhhh fixed!

--- carnifex.lisp
+++ carnifex.lisp
@@ -10,12 +10,6 @@
 (load "utils.lisp")
 (load "data.lisp")
 
-(defun usage (&key pname)
-  (lambda ()
-    (format t "usage: ~s <width> <height>~%"
-            pname)
-    (sb-ext:exit)))
-
 (defun life-loop ()
   (life-loop))
 
@@ -31,20 +25,19 @@
       (sdl:clear-display
        (sdl:color :r 30 :g 30 :b  57))
 	  (sdl:with-events ()
-		(:quit-event () t)
+					   (:quit-event () t)
 		(:key-down-event ()
 		  (when (or (sdl:key-down-p :sdl-key-q)
 					(sdl:key-down-p :sdl-key-escape))
 			(sdl:push-quit-event)))))))
 
 (defun main ()
-  (let* ((al (len *posix-argv*)))
-    (if (not (= al 3))
-        (usage :pname (nth 0 *posix-argv*))
-        nil))
-  (let* ((width (nth 1 *posix-argv*))
-         (height (nth 2 *posix-argv*)))
-    (format t "Width: ~f, Height: ~f~%"
-            width height))
-  (conway-automaton)
+  (usage :pname (car sb-ext:*posix-argv*))
+  (let* ((width (parse-integer (nth 1 *posix-argv*)))
+         (height (parse-integer (nth 2 *posix-argv*))))
+	(if (and (typep width 'integer)
+			 (typep height 'integer))
+		(format t "Width: ~d, Height: ~d~%"
+				width height)
+	  (usage :pname (nth 0 *posix-argv*))))
   (sb-ext:exit))
--- utils.lisp
+++ utils.lisp
@@ -6,4 +6,39 @@
   "Compute len af a list"
   (if list
       (1+ (len (cdr list)))
-      0))
+	0))
+
+(defun numargs (&rest arguments)
+  "Retrieve len of args"
+  (length arguments))
+
+(defmacro if-let (test bindings &body body)
+  (let ((bindings (mapcar #'(lambda (binding)
+                              (destructuring-bind (variable &optional then else)
+                                  (if (listp binding) binding (list binding))
+                                (list variable then else)))
+                          bindings)))
+    `(multiple-value-bind,
+	  (mapcar 'first bindings)
+         (if ,test
+             (values ,@(mapcar 'second bindings))
+             (values ,@(mapcar 'third bindings)))
+		 ,@body)))
+
+(defun usage (&key pname)
+  "Pretty printer for usage and arg checker"
+  (let ((banner "usage: ~s [-h --help] <width:int> <height:int>
+positional arguments:
+  width                 width of the grid
+  height                height of the grid
+optional arguments:
+  -h, --help            show this help message and exit~%"))
+	(if (or (loop for arg in *posix-argv*
+				  when (or (string-equal "-h" arg)
+						   (string-equal "--help" arg))
+				  return t)
+			(not (equal 3 (length *posix-argv*))))
+		(progn
+		  (format t banner pname)
+		  (sb-ext:exit))))
+	'continue) ; Don't kill thread
