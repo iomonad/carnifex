e611b5f59e14301594dbb8a5abfd7c5d4321f4e5
Clement Trosa <me@trosa.io>
2018-04-21 22:05:31 +0200

    refactoring project

---
+++ .dir-locals.el
@@ -0,0 +1,6 @@
+;;
+;; iomonad - <me@trosa.io>
+;;
+
+((nil . ((indent-tabs-mode . nil)))
+ (makefile-mode . ((indent-tabs-mode . t))))
--- carnifex.lisp
+++ carnifex.lisp
@@ -1,5 +1,6 @@
 ;; carnifex.lisp
-;; A Conway’s automaton.
+;; A Conway’s automaton written in lisp
+;; at 42.fr school.
 ;; 2018 - iomonad <me@trosa.io>
 
 (ql:quickload '(:lispbuilder-sdl)
@@ -7,19 +8,28 @@
 (ql:quickload '(:unix-opts)
               :silent t)
 
-(load "utils.lisp")
-(load "data.lisp")
-(load "conway.lisp")
+(defun init (&key pname)
+  "Check arguments and display usage when options is incorrect"
+    (let ((banner "usage: ~s <width:int> <height:int> [-h --help -i --invert -t --traces]
+positional arguments:
+  width                 width of the grid
+  height                height of the grid
+optional arguments:
+  -h, --help            show this help message and exit~%"))
+      (if (or (loop for arg in *posix-argv*
+                    when (or (string-equal "-h" arg)
+                             (string-equal "--help" arg))
+                    return t)
+              (not (< 2 (length *posix-argv*))))
+          (progn
+            (format t banner pname)
+            (sb-ext:exit))))
+    'continue)
 
 (defun main ()
-  (usage :pname (car sb-ext:*posix-argv*))
-  (let* ((width (parse-integer (nth 1 *posix-argv*)))
-         (height (parse-integer (nth 2 *posix-argv*))))
-	(if (and (typep width 'integer)
-			 (typep height 'integer))
-		(progn
-		  (format t "Width: ~d, Height: ~d~%"
-				  width height)
-		  (conway-automaton))
-	  (usage :pname (nth 0 *posix-argv*))))
-  (sb-ext:exit))
+  (init :pname
+    (car sb-ext:*posix-argv*))
+  (exit))
+
+(sb-int:with-float-traps-masked
+ (:invalid :inexact :overflow) (main))
